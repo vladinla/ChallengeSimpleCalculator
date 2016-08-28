38ada429c8a96f6d81fb70a9336663632e913ddf
Vladislav Golovin <golovin2011@gmail.com>
2016-08-28 09:09:44 +0300

    First commit - first version

---
+++ ChallengeSimpleCalculator/default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="ChallengeSimpleCalculator._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Simple Calculator</title>
+    <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet"/>
+    <link href="style.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <h1>Simple calculator</h1><br />
+        <br />
+        First Value:&nbsp;
+        <asp:TextBox ID="firstTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Second Value:&nbsp;
+        <asp:TextBox ID="secondTextBox" runat="server" OnTextChanged="TextBox2_TextChanged"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="plusButton" runat="server" OnClick="plusButton_Click" Text="+" />
+&nbsp;
+        <asp:Button ID="minusButton" runat="server" OnClick="minusButton_Click" Text="-" />
+&nbsp;
+        <asp:Button ID="multButton" runat="server" Text="*" OnClick="multButton_Click" />
+&nbsp;
+        <asp:Button ID="divisionButton" runat="server" Text="/" OnClick="divisionButton_Click" />
+        <br />
+        <br />
+        Result:&nbsp;
+        <asp:Label ID="resultLabel" runat="server" Font-Bold="True" Font-Size="Large"></asp:Label>
+        <br />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeSimpleCalculator/default.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeSimpleCalculator
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void plusButton_Click(object sender, EventArgs e)
+        {
+            int value1Plus = int.Parse(firstTextBox.Text);
+            int value2Plus = int.Parse(secondTextBox.Text);
+
+            int resultPlus = value1Plus + value2Plus;
+
+            resultLabel.Text = resultPlus.ToString();
+           
+        }
+
+        protected void minusButton_Click(object sender, EventArgs e)
+        {
+            int value1Minus = int.Parse(firstTextBox.Text);
+            int value2Minus = int.Parse(secondTextBox.Text);
+
+            int resultMinus = value1Minus - value2Minus;
+
+            resultLabel.Text = resultMinus.ToString();
+        }
+
+        protected void multButton_Click(object sender, EventArgs e)
+        {
+            int value1Mult = int.Parse(firstTextBox.Text);
+            int value2Mult = int.Parse(secondTextBox.Text);
+
+            int resultMult = value1Mult * value2Mult;
+
+            resultLabel.Text = resultMult.ToString();
+        }
+
+        protected void divisionButton_Click(object sender, EventArgs e)
+        {
+            double value1Div = int.Parse(firstTextBox.Text);
+            double value2Div = int.Parse(secondTextBox.Text);
+
+            double resultDivide = value1Div / value2Div;
+
+            resultLabel.Text = resultDivide.ToString();
+        }
+    }
+}
